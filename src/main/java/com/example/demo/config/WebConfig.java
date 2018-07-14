2de085bf9f2f53b945671663312ffee097a723f5
zk332 <1170110587@qq.com>
2018-07-14 14:41:47 +0800

    14:41

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -1,5 +1,7 @@
 package com.example.demo.config;
 
+import java.io.File;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -22,15 +24,19 @@ public class WebConfig extends WebMvcConfigurationSupport {
     }
 	 @Override
 	 public void addResourceHandlers(ResourceHandlerRegistry registry) {
-	       registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+	       //registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+	       String str="/src/main/resources/static/";
+	       File directory = new File("");
+	       String fullFilePath = directory.getAbsolutePath();
+	       registry.addResourceHandler("/**").addResourceLocations("file:"+fullFilePath+str);
 	       super.addResourceHandlers(registry);
 	    }
 	 @Override
-	    public void addInterceptors(InterceptorRegistry registry) {
+	 public void addInterceptors(InterceptorRegistry registry) {
 	        // addPathPatterns 用于添加拦截规则
 	        // excludePathPatterns 用户排除拦截
 	        // 映射为 user 的控制器下的所有映射
-	 registry.addInterceptor(myInterceptor).addPathPatterns("/login").excludePathPatterns("/index", "/");
+		 registry.addInterceptor(myInterceptor).addPathPatterns("/login").excludePathPatterns("/index", "/");
 	        super.addInterceptors(registry);
 	    }
 
